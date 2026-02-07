fd3e8793474a563c85330cafc19aafe9b95a6d6d
ivnwynn <sanjuanivan0522@gmail.com>
2026-02-07 23:55:53 +0800

    Delete greetings.py

--- greetings.py
+++
@@ -1,8 +0,0 @@
-def greetings():
-    name = input("Enter your name please: ")
-    if name == "ivan":
-        print("Welcome! How may I help you today sir?")
-    else:
-        print("Unauthorized Personel is Prohibited")
-
-greetings()
